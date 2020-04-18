025eeac55e807eae7bc5434795712ae279583b2a
roksdm <roks.dm.hm@gmail.com>
2020-04-18 13:07:41 +0300

    add form

---
+++ file.js
@@ -0,0 +1,10 @@
+function show() {
+    alert("Заполните пожалуйста форму!");
+  }
+    var x = document.getElementsByClassName("btnflip");
+    x.onclick = function () {
+    document.body.innerHTML = Date();
+}
+
+element.addEventListener("click", myFunction);
+element.addEventListener("mouseover", myFunction);
