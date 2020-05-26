4f08ca2b70c0287f1498bfa78215fbf0e95321c2
roksdm <roks.dm.hm@gmail.com>
2020-05-26 16:47:52 +0300

    +weather

--- file.js
+++ file.js
@@ -13,3 +13,21 @@
     node.classList.remove('.contact form');
   }
 })();
+
+
+fetch('https://api.openweathermap.org/data/2.5/weather?id=703448&appid=35727347632d7516f055c2ea2466f3e1')
+  .then(function (resp) { return resp.json() })
+  .then(function (data) {
+    console.log(data);
+    document.querySelector('.package-name').textContent = data.name;
+    document.querySelector('.price').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+    document.querySelector('.disclaimer').textContent = data.weather[0]['description'];
+    document.querySelector('.features li').innerHTML = '<img src ="https://openweathermap.org/img/wn/' + data.weather[0]['icon'] + '@2x.png">';
+
+  })
+  .catch(function () {
+
+  });
+
+
+  //https://openweathermap.org/img/wn/02d@2x.png
